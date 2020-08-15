dd62f027217b7ac9381f9d81ea86a576415f7d1d
IX-Peerapat <peerapat.me@rmuti.ac.th>
2020-08-16 02:56:29 +0700

    Update classUser & add classUserDao

--- app/src/main/java/com/example/firstapp/Database/User.kt
+++ app/src/main/java/com/example/firstapp/Database/User.kt
@@ -1,4 +1,12 @@
 package com.example.firstapp.Database
 
-class User {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val Id: Int,
+    val fName: String,
+    val phoneNumber: Int
+)
---
+++ app/src/main/java/com/example/firstapp/Database/UserDao.kt
@@ -0,0 +1,4 @@
+package com.example.firstapp.Database
+
+interface UserDao {
+}
