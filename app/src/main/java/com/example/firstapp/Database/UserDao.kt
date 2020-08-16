b72f0552decbb0f298d12a5deeff1787763a1962
IX-Peerapat <peerapat.me@rmuti.ac.th>
2020-08-16 11:56:12 +0700

    Update Database

--- app/src/main/java/com/example/firstapp/Database/UserDao.kt
+++ app/src/main/java/com/example/firstapp/Database/UserDao.kt
@@ -1,4 +1,14 @@
 package com.example.firstapp.Database
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
 interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addUser(user: User)
+
+    @Query("SELECT * FROM user_table ORDER BY id ASC")
+    fun readAllDate(): LiveData<List<User>>
 }
