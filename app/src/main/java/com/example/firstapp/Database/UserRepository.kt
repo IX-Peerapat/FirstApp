dce317fed00ffe373b7933e6c5b3421879ea67db
IX-Peerapat <peerapat.me@rmuti.ac.th>
2020-08-16 12:18:47 +0700

    Add Fragment

--- app/src/main/java/com/example/firstapp/Database/UserRepository.kt
+++ app/src/main/java/com/example/firstapp/Database/UserRepository.kt
@@ -1,4 +1,12 @@
 package com.example.firstapp.Database
 
-class UserRepository {
+import androidx.lifecycle.LiveData
+
+class UserRepository (private val userDAO: UserDao){
+
+    val readAllDate: LiveData<List<User>> = userDAO.readAllDate()
+
+    suspend fun addUser(user: User){
+        userDAO.addUser(user)
+    }
 }
